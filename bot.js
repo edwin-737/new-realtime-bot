67553c8aca72aabf6f1d5ab2c5b8c9ace33b7d5f
edwin-737 <76090138+edwin-737@users.noreply.github.com>
2023-05-02 02:02:46 +0800

    fix: use TeamsActivityHandler instead of ActivityHandler

--- bot.js
+++ bot.js
@@ -1,12 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, MessageFactory, TeamsActivityHandler } = require('botbuilder');
 
-class EchoBot extends ActivityHandler {
+export class EchoBot extends TeamsActivityHandler {
     constructor() {
         super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onTeamsChannelCreatedEvent(async (channelInfo, teamInfo, turnContext, next) => {
+            const card = CardFactory.heroCard('Channel Created', `${channelInfo.name} is the Channel created`);
+            const message = MessageFactory.attachment(card);
+            // Sends a message activity to the sender of the incoming activity.
+            await turnContext.sendActivity(message);
+            await next();
+        });
         this.onMessage(async (context, next) => {
 
             const replyText = `Echo: ${context.activity.text}`;
@@ -15,20 +21,36 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
-
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome! New deployment';
-            console.log('onMembersAdded called');
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
     }
 }
+
+// class EchoBot extends ActivityHandler {
+//     constructor() {
+//         console.log('constructor called');
+//         super();
+//         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+//         this.onMessage(async (context, next) => {
+
+//             const replyText = `Echo: ${context.activity.text}`;
+//             console.log('onMessage called ' + replyText);
+//             await context.sendActivity(MessageFactory.text(replyText, replyText));
+//             // By calling next() you ensure that the next BotHandler is run.
+//             await next();
+//         });
+
+//         this.onMembersAdded(async (context, next) => {
+//             const membersAdded = context.activity.membersAdded;
+//             const welcomeText = 'Hello and welcome! New deployment';
+//             console.log('onMembersAdded called');
+//             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+//                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
+//                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+//                 }
+//             }
+//             // By calling next() you ensure that the next BotHandler is run.
+//             await next();
+//         });
+//     }
+// }
 
 module.exports.EchoBot = EchoBot;
