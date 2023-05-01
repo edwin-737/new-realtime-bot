5f06ff27d2a1a98bb68c487a6d703b67ee37473c
edwin-737 <76090138+edwin-737@users.noreply.github.com>
2023-05-01 14:00:12 +0800

    fix: minor log  bug

--- bot.js
+++ bot.js
@@ -8,9 +8,9 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            console.log('onMessage called ' + replyText);
 
             const replyText = `Echo: ${context.activity.text}`;
+            console.log('onMessage called ' + replyText);
             await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
