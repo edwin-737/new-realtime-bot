62e7666e9a3c882a49e2c2a4acfef60064e48c49
edwin-737 <76090138+edwin-737@users.noreply.github.com>
2023-05-05 11:23:22 +0700

    feat: send anonymous message using the bot, to a specified channel

--- bot.js
+++ bot.js
@@ -1,23 +1,38 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { MessageFactory, TeamsActivityHandler } = require('botbuilder');
+const {
+    MessageFactory,
+    TeamsActivityHandler,
+    TeamsInfo
+} = require('botbuilder');
 
 class EchoBot extends TeamsActivityHandler {
     constructor() {
         super();
         this.onMessage(async (context, next) => {
-
             const replyText = `Echo: ${context.activity.text}`;
             console.log('onMessage called\n' + replyText);
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+
+            //for now test using a known teamsChannelId
+            //Replace with teamsChannelId retrieved from some database 
+            const teamsChannelId = '19:68d2027a02114599be34f2bf95901699@thread.tacv2';
+            const question = context.activity.text;
+
+            // //send the message to the desired channel
+            const activity = MessageFactory.text(question);
+            // const [reference] = await TeamsInfo.sendMessageToTeamsChannel(context, activity, teamsChannelId, process.env.MicrosoftAppId);
+            // await context.adapter.continueConversationAsync(process.env.MicrosoftAppId, reference, async turnContext => {
+            //     await turnContext.sendActivity(MessageFactory.text(question));
+            // });
+
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
         // Listen to MembersAdded event, view https://docs.microsoft.com/en-us/microsoftteams/platform/resources/bot-v3/bots-notifications for more events
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = "welcome, the bot works on teams!";
+            const welcomeText = "Welcome, this bot is for testing.";
             console.log('onMembers added called\n' + welcomeText);
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id) {
@@ -28,35 +43,6 @@ class EchoBot extends TeamsActivityHandler {
             await next();
         });
     }
+
 }
-
-// class EchoBot extends ActivityHandler {
-//     constructor() {
-//         console.log('constructor called');
-//         super();
-//         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-//         this.onMessage(async (context, next) => {
-
-//             const replyText = `Echo: ${context.activity.text}`;
-//             console.log('onMessage called ' + replyText);
-//             await context.sendActivity(MessageFactory.text(replyText, replyText));
-//             // By calling next() you ensure that the next BotHandler is run.
-//             await next();
-//         });
-
-//         this.onMembersAdded(async (context, next) => {
-//             const membersAdded = context.activity.membersAdded;
-//             const welcomeText = 'Hello and welcome! New deployment';
-//             console.log('onMembersAdded called');
-//             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-//                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-//                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-//                 }
-//             }
-//             // By calling next() you ensure that the next BotHandler is run.
-//             await next();
-//         });
-//     }
-// }
-
 module.exports.EchoBot = EchoBot;
